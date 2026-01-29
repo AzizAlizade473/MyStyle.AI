0e4759f967488ec055f97148fee74ff8e88ee217
Muhi2007 <muhammadteymurlu07@gmail.com>
2026-01-29 23:51:22 +0400

    Profile page fixage; need improvement in JS

--- backend/users/views.py
+++ backend/users/views.py
@@ -16,6 +16,7 @@ def register_user(request):
         if serializer.is_valid():
             serializer.save()
             return Response({'message':"User created successfully"}, status=status.HTTP_201_CREATED)
+        print(serializer.errors)
         return Response(serializer.errors ,status=status.HTTP_400_BAD_REQUEST)
 
 class ManageProfileView(generics.RetrieveUpdateAPIView):
