7d1e7901447352999de47703609f761193e7543b
Muhi2007 <muhammadteymurlu07@gmail.com>
2026-02-01 23:06:24 +0400

    Home(desktop) connected to backend

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path('api/auth/', include('users.urls')),
 
     # Frontend Pages
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('', TemplateView.as_view(template_name='home(desktop).html'), name='home'),
     path('login/', TemplateView.as_view(template_name='login(desktop).html'), name='login'),
-    path('new_front/', TemplateView.as_view(template_name='index(desktop).html'), name='new_front'),
+    path('new_front/', TemplateView.as_view(template_name='index(desktop).html'), name='new_front'), # Testing purposes
     path('register/', TemplateView.as_view(template_name='register(desktop).html'), name='register'),
     path('profile/', TemplateView.as_view(template_name='profile(desktop).html'), name='profile_page'),
 ]
